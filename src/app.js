5c4ec038af2c0ee6e0627faf48de99aa8c9666b7
Sonali <patilsonalias002@gmail.com>
2024-01-27 20:41:53 +0530

    add route

--- src/app.js
+++ src/app.js
@@ -9,6 +9,10 @@ app.get("/", (req,res)=>{
     res.send("user management");
 })
 
+app.get('/get', (req,res)=>{
+    res.send("user mang");
+})
+
 app.listen(PORT, ()=>{
     console.log(`server is running on port ${PORT}`);
 })
